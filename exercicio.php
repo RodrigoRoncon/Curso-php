8fc6aa2e42e22bded0c6120330104cc7b71e99da
RodrigoRoncon <100783231+RodrigoRoncon@users.noreply.github.com>
2024-01-30 22:37:17 -0300

    adicionado arquivo php

---
+++ exercicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício</title>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&family=Single+Day&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Vizualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+           
+        </div>
+    </main>
+    <footer class="rodape">
+        Cod3r & Alunos : <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
